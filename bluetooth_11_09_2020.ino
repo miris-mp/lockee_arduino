45c168122c227e18766d1f353c20c619b09a75d9
miris-mp <miriam.petrocchi94@gmail.com>
2020-11-02 11:37:18 +0100

    changes to read correctly the send request

--- bluetooth_11_09_2020.ino
+++ bluetooth_11_09_2020.ino
@@ -1,88 +1,34 @@
 #include <SoftwareSerial.h>
 
-// Define the data transmit/receive pins in Arduino
+int rxPin = 3;
+int txPin = 2;
+SoftwareSerial bluetooth(rxPin, txPin);
 
-#define TxD 2
+String message; //string that stores the incoming message
 
-#define RxD 3
-
-SoftwareSerial BTSerial(RxD, TxD); // RX, TX for Bluetooth
-
-void setup() {
-  Serial.begin(9600); // For the IDE monitor Tools -> Serial Monitor
-  BTSerial.begin(9600); // For Bluetooth
-  while (!Serial) {
-    Serial.println("AT commands: okay");
-  }
+void setup()
+{
+  Serial.begin(9600); //set baud rate
+  bluetooth.begin(9600); //set baud rate
 }
-int counter = 0;
- const byte buflen = 10;
-    char buf[ buflen ];
-    int i = 0;
-    bool res = false;
-    
-void loop() {
- 
-  if (BTSerial.available()) {
-    Serial.println("Primo");
-    //Serial.write(BTSerial.read());  
-    buf[counter] = BTSerial.read();
-    counter++;
-    
-  } else if (counter != i){
-    Serial.println("Secondo");
-    for (; i < counter; i++) {
-      //Serial.print(buf[i]); 
+
+void loop()
+{
+  while(bluetooth.available()){
+    message+=char(bluetooth.read());
+  }
+  if(!bluetooth.available())
+  {
+    if(message!="")
+    {//if data is available
+      Serial.println(message); //show the data
+      message=""; //clear the data
     }
-  } else if (counter == i && counter != 0) {
-    Serial.println("Entro pure qua");
-    res = true;
-    counter = 0;
-    i = 0;
   }
+  delay(5000); //delay
 
-  if (res) {
-    BTSerial.write("messaggio privato");
+   if (Serial.available())
+  {
+    bluetooth.write(Serial.read());
   }
-  
-    if (Serial.available()) {
-    BTSerial.write(Serial.read());
-    
-  }
- 
-  /*
-// put your main code here, to run repeatedly:
-boolean isValidInput; 
-do { 
- 
-  byte c; // get the next character from the bluetooth serial port
-
-  while (!BTSerial.available()); 
-    c = BTSerial.read(); // Execute the option based on the character recieved
-    Serial.print(c); // Print the character received to the IDE serial monitor
-
-    switch ( c ) {
-
-    Serial.write("a");
-      case 'a': // You've entered a
-        // Do the code you need when 'a' is received.....
-        BTSerial.println( "You've entered an 'a'" );
-        isValidInput = true;
-      break;
-
-      case 'b': // You've entered b
-        // Do the code you need when 'a' is received.....
-        BTSerial.println( "You've entered an 'b'" );
-        isValidInput = true;
-      break;
-
-      default:
-        // Do the code you need when any other character is received.....
-        BTSerial.println( "Please enter 'a' or 'b'" );
-        isValidInput = false;
-      break;
-    }
-
-  }while ( isValidInput == true ); // Repeat the loop
-  */
 }
